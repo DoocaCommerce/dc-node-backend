d4a80bfc7033de924db03a0cf5e1fdb948aee093
tiagoroosdooca <83605924+tiagoroosdooca@users.noreply.github.com>
2021-12-30 14:10:26 -0300

    feat: criado index do Apollo

---
+++ src/Graphql/Server/Apollo/index.ts
@@ -0,0 +1,4 @@
+export { ApolloProcess } from './ApolloProcess'
+export { ApolloServerContext } from './ApolloServerContext'
+export { ApolloServerResolvers } from './ApolloServerResolvers'
+export { ApolloServerTypeDefs } from './ApolloServerTypeDefs'
