d13a0e18a7e3c26d5173be913d25948c57b9bf3e
tiagoroosdooca <83605924+tiagoroosdooca@users.noreply.github.com>
2022-02-11 14:28:02 -0300

    Fix: no payload do ClientConfig agora recebe json

--- src/Http/Client/ClientConfig.ts
+++ src/Http/Client/ClientConfig.ts
@@ -1,8 +1,9 @@
+import { Json } from '../../Base'
 import { HttpMethodEnum } from '../HttpMethodEnum'
 
 export interface ClientConfig {
     method: HttpMethodEnum
     url: string
-    payload?: object
+    payload?: Json
     headers?: Record<string, string>
 }
