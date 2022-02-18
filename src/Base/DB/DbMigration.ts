a8ebca05c01c4d309b86548911521e3fdb4e8fd8
tiagoroosdooca <83605924+tiagoroosdooca@users.noreply.github.com>
2022-02-18 17:05:18 -0300

    fix: up e down deveriam ter promise de retorno

--- src/Base/DB/DbMigration.ts
+++ src/Base/DB/DbMigration.ts
@@ -3,6 +3,6 @@ import { DbConnection } from '.'
 export abstract class DbMigration<T extends DbConnection> {
     constructor(protected conn: T) {}
 
-    abstract up(): void
-    abstract down(): void
+    abstract up(): Promise<void>
+    abstract down(): Promise<void>
 }
