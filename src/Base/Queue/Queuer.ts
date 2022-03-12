a36a2a9d0d1d815c8f30ddfab5cab753cccf2503
tiagoroosdooca <83605924+tiagoroosdooca@users.noreply.github.com>
2022-03-12 16:12:01 -0300

    feat: modificado o tipo do queuer

--- src/Base/Queue/Queuer.ts
+++ src/Base/Queue/Queuer.ts
@@ -1,5 +1,5 @@
 import { QueueItem } from './QueueItem'
 
-export interface Queuer<I extends QueueItem> {
+export interface Queuer<I extends QueueItem = QueueItem> {
     add(item: I): Promise<Queuer<I>>
 }
