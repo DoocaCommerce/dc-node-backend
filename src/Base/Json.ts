c06341923441ec566294eb45d7c2ced218310e75
tiagoroosdooca <83605924+tiagoroosdooca@users.noreply.github.com>
2022-04-30 15:03:49 -0300

    fix: adicinado any no JSON

--- src/Base/Json.ts
+++ src/Base/Json.ts
@@ -1,4 +1,13 @@
-export type Json = string | number | boolean | JsonObject | JsonArray | Json[] | null
+export type Json =
+    | string
+    | number
+    | boolean
+    | JsonObject
+    | JsonArray
+    | Json[]
+    | null
+    | undefined
+    | any
 
 export interface JsonObject {
     [key: string]: Json
